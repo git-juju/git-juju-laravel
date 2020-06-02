5b6377893ad37c5311280589b4e83915bb465766
Xiao juju <896412698@qq.com>
2020-06-02 02:44:10 +0000

    完成（Updated database configuration）

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 
+$db_config = get_db_config();
 return [
 
     /*
@@ -15,8 +16,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-
+    //'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -66,11 +67,15 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host'     => $db_config['host'],
+            //'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -6,7 +6,7 @@
                 <a class="nav-link" href="{{url('help')}}">帮助</a>
             </li>
             <li class="nav-item" >
-                <a class="nav-link" href="#">登录</a>
+                <a class="nav-link" href="{{url( 'signup')}}">登录/注册</a>
             </li>
         </ul>
     </div>
